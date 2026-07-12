e72e859d7738a04a99bcb7f695f5af3dad890cea
helencf93-oss <helencf93@gmail.com>
2026-07-12 20:38:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,112 +1,75 @@
-// ======================================
-// COUNTDOWN - OUR NEXT CHAPTER ❤️
-// DATA: 03/01/2027
-// ======================================
-
-
-const targetDate = new Date("2027-01-03T00:00:00");
+const target = new Date("2027-01-03T00:00:00");
 
 
 const countdown = document.getElementById("countdown");
+
 const finalMessage = document.getElementById("finalMessage");
 
-const daysEl = document.getElementById("days");
-const hoursEl = document.getElementById("hours");
-const minutesEl = document.getElementById("minutes");
-const secondsEl = document.getElementById("seconds");
+
+function update(){
+
+
+const now = new Date();
+
+
+const distance = target - now;
 
 
 
-function updateCountdown(){
+if(distance <= 0){
 
 
-    const now = new Date();
-
-    const difference = targetDate - now;
+countdown.style.display="none";
 
 
-
-    // Quando chegar a data
-
-    if(difference <= 0){
+finalMessage.style.display="block";
 
 
-        countdown.style.opacity = "0";
-
-
-        setTimeout(()=>{
-
-
-            countdown.style.display = "none";
-
-
-            finalMessage.innerHTML = `
-            
-            ❤️
-            <br>
-            <span>We made it.</span>
-
-            `;
-
-
-            finalMessage.classList.add("show");
-
-
-        },1000);
-
-
-
-        return;
-
-    }
-
-
-
-    const days = Math.floor(
-        difference / (1000*60*60*24)
-    );
-
-
-    const hours = Math.floor(
-        (difference % (1000*60*60*24))
-        /(1000*60*60)
-    );
-
-
-    const minutes = Math.floor(
-        (difference % (1000*60*60))
-        /(1000*60)
-    );
-
-
-    const seconds = Math.floor(
-        (difference % (1000*60))
-        /1000
-    );
-
-
-
-    daysEl.textContent =
-        String(days).padStart(3,"0");
-
-
-    hoursEl.textContent =
-        String(hours).padStart(2,"0");
-
-
-    minutesEl.textContent =
-        String(minutes).padStart(2,"0");
-
-
-    secondsEl.textContent =
-        String(seconds).padStart(2,"0");
+return;
 
 
 }
 
 
 
-updateCountdown();
+const days = Math.floor(distance/(1000*60*60*24));
 
 
-setInterval(updateCountdown,1000);
+const hours = Math.floor(
+(distance%(1000*60*60*24))/(1000*60*60)
+);
+
+
+const minutes = Math.floor(
+(distance%(1000*60*60))/(1000*60)
+);
+
+
+const seconds = Math.floor(
+(distance%(1000*60))/1000
+);
+
+
+
+document.getElementById("days").innerHTML =
+String(days).padStart(3,"0");
+
+
+document.getElementById("hours").innerHTML =
+String(hours).padStart(2,"0");
+
+
+document.getElementById("minutes").innerHTML =
+String(minutes).padStart(2,"0");
+
+
+document.getElementById("seconds").innerHTML =
+String(seconds).padStart(2,"0");
+
+
+}
+
+
+update();
+
+setInterval(update,1000);
